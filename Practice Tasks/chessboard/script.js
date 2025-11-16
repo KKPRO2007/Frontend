ec438a2e6e3cdf0e9c85d11dff7c8c608fc2d96e
kk professional <kkpro2007@gmail.com>
2025-11-16 21:27:42 +0530

    task 1 and 2

--- Practice Tasks/chessboard/script.js
+++ Practice Tasks/chessboard/script.js
@@ -1,8 +1,9 @@
 const chessboard = document.getElementById('chessboard');
-
-for (let i = 0; i < 64; i++) {
-  const div = document.createElement('div');
-  div.className = 'square';
-  div.style.backgroundColor = (Math.floor(i / 8) + i) % 2 == 0 ? '#f0d9b5' : '#593f2aff';
-  chessboard.appendChild(div);
+for(let r=0; r<8; r++){
+  for(let c=0; c<8; c++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.backgroundColor=(r+c) %2===0? 'white': 'black';
+    chessboard.appendChild(square);
+  }
 }
